4c0d96dd796739efdca5f2b88a47eac211ca364c
acer-haitao <acer_yuhaitao@163.com>
2016-12-15 22:16:53 +0800

    添加Python监控花生壳客户端是否掉线

---
+++ oray_ckeck.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import re
+
+while(1) :
+    var = os.popen("sudo oraynewph status | grep ONLINE | sed -n \"1,0p\"").read()
+    #print var
+    #list = re.match(r'RUNSTATUS= ONLINE',var)
+    #str = list.group()
+    #print str
+    if ('RUNSTATUS= ONLINE\n' == var) :
+        print "花生壳客户端已经登录成功"
+        os.system("sleep 60")
+    else :
+        print "正在重启花生壳客户端"
+        os.system("sudo oraynewph start")
+        os.system("sleep 30")
+
