df497ba3a3f4a5ca46df2ec1597dd60cd22eb4e9
acer-haitao <acer_yuhaitao@163.com>
2017-07-04 15:04:21 +0800

    2017-7-4

---
+++ www/cgi-bin/getsite_mysql.php
@@ -0,0 +1,58 @@
+﻿#!/etc/php/bin/php
+
+<?php
+echo "X-Powered-By: PHP/ 5.6.27\n";
+echo "Content-type: text/html\n\n";
+?>
+
+
+
+<?php
+Error_reporting(E_ALL);
+INI_Set('display_errors','on');
+$q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
+if(empty($q)) {
+   echo '请选择一个网站';
+ //  exit;
+}
+
+$con = mysqli_connect('localhost','root','haitao');
+if (!$con)
+{
+   	die('Could not connect: ' . mysqli_error($con));
+}
+// 选择数据库
+mysqli_select_db($con,"mynetdb");
+// 设置编码，防止中文乱码
+mysqli_set_charset($con, "utf8");
+ 
+$sql="SELECT * FROM mynet WHERE Num = '".$q."'";
+//$sql="SELECT * FROM websites WHERE name = 'Google'";
+ 
+$result = mysqli_query($con,$sql);
+if (!$result) {
+	 printf("Error: %s\n", mysqli_error($con));
+	  exit();
+}
+echo "<table border='1' width='80%' align='center'>
+	<tr>
+	<th>编号</th>
+	<th>网站名称</th>
+	<th>网站地址</th>
+	<th>添加时间</th>
+	</tr>";
+ 
+while($row = mysqli_fetch_array($result))
+{
+	    echo "<tr>";
+	    echo "<td>" . $row['Num'] . "</td>";
+		echo "<td>" . $row['Name'] . "</td>";
+		echo "<td>" . $row['URL'] . "</td>";
+		echo "<td>" . $row['Time'] . "</td>";
+		echo "</tr>";
+}
+echo "</table>";
+ 
+mysqli_close($con);
+?>
+
---
+++ www/cgi-bin/phpinfo.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+echo "X-Powered-By: PHP/5.6.27\n";
+echo "Content-type: text/html\n\n";
+?>
+<html>
+<body>
+Hello!
+<?php
+phpinfo();
+?>
+</body>
+</html>
