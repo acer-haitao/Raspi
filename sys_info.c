1ba6138e56a717baaa5e50a881cbaea8fb109c0c
acer-haitao <acer_yuhaitao@163.com>
2018-05-26 17:26:42 +0800

    add right.html

---
+++ sys_info.c
@@ -0,0 +1,21 @@
+成功登录:2018-05-24 22:12:03 
+主机信息:Linux raspberrypi 4.1.19-v7+ #858 SMP Tue Mar 15 15:56:00 GMT 2016 armv7l GNU/Linux
+无线IP:192.168.1.9
+有线IP:
+磁盘信息：
+ Filesystem      Size  Used Avail Use% Mounted on
+/dev/root       7.2G  4.8G  2.1G  70% /
+devtmpfs        427M     0  427M   0% /dev
+tmpfs           432M     0  432M   0% /dev/shm
+tmpfs           432M  5.9M  426M   2% /run
+tmpfs           5.0M  4.0K  5.0M   1% /run/lock
+tmpfs           432M     0  432M   0% /sys/fs/cgroup
+/dev/mmcblk0p1   60M   20M   40M  34% /boot
+tmpfs            87M     0   87M   0% /run/user/1000 
+ boa服务器信息:  707 ?        00:00:00 boa 
+花生壳客户端登录信息:
+ 
+ 
+CPU温度:53692 
+VNC开启状态:
+
